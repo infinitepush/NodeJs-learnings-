28764876337d0d72cca32a4e52c17e6724fe11fb
Piyush Tiwari <piyush89101@gmail.com>
2025-07-23 21:38:35 +0530

    codes updated

--- L7 (EXPRESS JS Intro)/practice/practice1.js
+++ L7 (EXPRESS JS Intro)/practice/practice1.js
@@ -2,7 +2,7 @@
 /*  Add two dummy middleware that logs requests paths and request method respectively.
     Add a third middleware that returns a response.
     Now add handling using two more middleware that handle path /, a requests to /contact-us page.
-    Contact us should return a form with name and email as input fields that submits to /contact-us page also.
+    Contact us should return a form with name and email as input fields that submits to /contact-us page also. 
     Also handle POST incoming request to /contact-us path using a sepearte middleware
 */
 
