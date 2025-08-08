c8770a4b336e6023fcf88035fd008a43622ae7e9
Piyush Tiwari <piyush89101@gmail.com>
2025-08-08 07:15:09 +0530

    Need of Dynamic UI

---
+++ L10 Dynamic UI using EJS/index.js
@@ -0,0 +1,3 @@
+//10 Dynamic Ui using EJS: Till now what we have seen was static UI but in dynamic UI pages the content are changing.. in this chapter we will learn how to deal with dynamic UI
+
+//10.1 Need of Dynamic UI: For building better applications we need Dynamic UI. Read notes for better understanding and points.
